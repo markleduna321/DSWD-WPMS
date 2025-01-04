8ee83d41c6a3c733e517ac84d5260ab60857af4b
markleduna321 <markharvey.leduna@gmail.com>
2025-01-04 17:14:39 +0800

    Contents Page Section UI

--- resources/js/app/pages/admin/contents/page.jsx
+++ resources/js/app/pages/admin/contents/page.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import AdminLayout from '../layout'
+import ContentsSection from './_sections/contents-section'
 
 export default function ContentPage() {
   return (
     <AdminLayout>
-        <div>ContentPage</div>
+        <ContentsSection/>
     </AdminLayout>
     
   )
